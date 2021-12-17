2655952530b0b2b73505877d0068b8a89757bb72
Lucas Gomes <lucasgomesc582@gmail.com>
2021-12-17 13:20:08 -0400

    persistencia de dados com mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://lgc:12345qwert@cluster0.xn7gc.mongodb.net/smartranking?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
+    
+    JogadoresModule],
   controllers: [],
   providers: [],
 })
--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,63 +1,51 @@
 /* eslint-disable prettier/prettier */
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
-import * as uuid from 'uuid'
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class JogadoresService {
 
-    private jogadores: Jogador[] = []
-
-    private readonly logger = new Logger(JogadoresService.name)
+    constructor(@InjectModel('Jogador') private readonly jogadorModel: Model<Jogador>) {}
 
     async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
 
         const { email } = criarJogadorDto
 
-        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email);
+        const jogadorEncontrado = await this.jogadorModel.findOne({email}).exec();
 
         if(jogadorEncontrado) {
-            this.atualizar(jogadorEncontrado, criarJogadorDto);
+            this.atualizar(criarJogadorDto);
         } else {
             this.criar(criarJogadorDto);
         }
     }
 
     async consultarTodosJogadores(): Promise<Jogador[]> {
-        return await this.jogadores;
+        return await this.jogadorModel.find().exec();
     }
 
-    private criar(criarJogadorDto: CriarJogadorDto): void {
-        const { nome, telefoneCelular, email } = criarJogadorDto
-        const jogador: Jogador = {
-            _id: uuid(),
-            nome,
-            telefoneCelular,
-            email,
-            ranking: 'A',
-            posicaoRanking: 1,
-            urlFotoJogador: 't'
-        }
-        this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
-        this.jogadores.push(jogador);
+    private async criar(criarJogadorDto: CriarJogadorDto): Promise<Jogador> {
+        
+        const jogadorCriado = new this.jogadorModel(criarJogadorDto)
+        return await jogadorCriado.save();
     }
 
-    private atualizar(jogadorEncontrado: Jogador, criarJogadorDto: CriarJogadorDto): void {
-        const { nome } = criarJogadorDto
-        jogadorEncontrado.nome = nome;
+    private async atualizar(criarJogadorDto: CriarJogadorDto): Promise<Jogador> {
+        return await this.jogadorModel.findOneAndUpdate({email: criarJogadorDto.email}, {$set: criarJogadorDto}).exec()
     }
 
     async consultarJogadorPeloEmail(email: string): Promise<Jogador> {
-        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email);
+        const jogadorEncontrado = await this.jogadorModel.findOne({email}).exec();
         if(!jogadorEncontrado) {
             throw new NotFoundException(`Jogador com e-mail ${email} não encontrado.`)
         }
         return jogadorEncontrado;
     }
 
-    async deletarJogador(email: string): Promise<void> {
-        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email);
-        this.jogadores = this.jogadores.filter(jogador => jogador.email !== jogadorEncontrado.email)
+    async deletarJogador(email: string): Promise<any> {
+        return await this.jogadorModel.remove({email}).exec()
     }
 }
